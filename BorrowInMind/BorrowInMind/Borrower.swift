485d6bc48b9448f0963021f0c54d13e0fbcbcc49
st81801 <st81801@yahoo.com.tw>
2018-06-27 15:21:53 +0800

    Add files via upload

---
+++ BorrowInMind/BorrowInMind/Borrower.swift
@@ -0,0 +1,13 @@
+//
+//  Borrower.swift
+//  BorrowInMind
+//
+//  Created by User17 on 2018/6/27.
+//  Copyright © 2018年 User17. All rights reserved.
+//
+
+import Foundation
+
+struct borrower {
+    var name:String
+}
